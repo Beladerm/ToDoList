62a03ff925d186f9a62e519ba4ade146ca3b3ef8
Beladerm <zoga_smle@mail.ru>
2023-03-29 20:24:33 +0300

    Start to-do-list project

---
+++ app/src/main/java/ru/ms/stu/todolist_va/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.ms.stu.todolist_va
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.ms.stu.todolist_va.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+    }
+}
